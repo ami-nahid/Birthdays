a01189eaa6329daf9fcb4e676eba84264e7ff43e
Ami <ami.m.nahid@gmail.com>
2025-07-25 11:14:47 -0400

    added delete function to delete friends data

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -20,13 +20,18 @@ struct ContentView: View {
         NavigationStack {
             
             //display birthdays
-            List (friends) { friend in
-                HStack{
-                    Text (friend.name)
-                    Spacer()
-                    Text (friend.birthday, format: .dateTime.month(.wide).day().year())
+            List {
+                ForEach(friends) {friend in
+                    HStack{
+                        Text (friend.name)
+                        Spacer()
+                        Text (friend.birthday, format: .dateTime.month(.wide).day().year())
+                    }
                 }
+            .onDelete(perform: deleteFriend)
             }
+           
+
             .navigationTitle("Birthdays")
             .safeAreaInset (edge: .bottom){
                 
@@ -52,6 +57,14 @@ struct ContentView: View {
                 .padding ()
                 .background(.bar)
             }
+        }
+    }
+    
+    //function to have option to delete friend
+    func deleteFriend (at offsets: IndexSet){
+        for index in offsets{
+            let friendToDelete = friends [index]
+            context.delete (friendToDelete)
         }
     }
 }
